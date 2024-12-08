00dd0b9597d2d4ed81ba87faf432dd29b90bccc7
dhikrama <dhikrama.creator@gmail.com>
2024-12-08 10:22:46 -0800

    faf

--- app.vue
+++ app.vue
@@ -18,6 +18,19 @@ useHead({
       rel: 'canonical',
       href: `${baseUrl}${route.path}`, // URL dinamis berdasarkan path
     },
+
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=AW-16813484049',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-16813484049');
+      `
+    }
   ]
 });
 </script>
