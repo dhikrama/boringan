d9875b0156544282977c7184989e909451a7f3d1
dhikrama <dhikrama.creator@gmail.com>
2024-12-10 18:08:08 -0800

    commit

--- components/home/AboutComponent.vue
+++ components/home/AboutComponent.vue
@@ -9,34 +9,6 @@
           alt="Happy Community"
           class="rounded-xl shadow-lg w-full"
         />
-        <!-- Card Komunitas -->
-        <div
-          class="absolute bottom-6 left-6 bg-white rounded-lg shadow-md p-4 flex items-center gap-4"
-        >
-          <!-- Avatar Group -->
-          <div class="flex -space-x-2">
-            <img
-              src="https://i.pravatar.cc/40?img=1"
-              alt="Member 1"
-              class="w-10 h-10 rounded-full border-2 border-white"
-            />
-            <img
-              src="https://i.pravatar.cc/40?img=2"
-              alt="Member 2"
-              class="w-10 h-10 rounded-full border-2 border-white"
-            />
-            <img
-              src="https://i.pravatar.cc/40?img=3"
-              alt="Member 3"
-              class="w-10 h-10 rounded-full border-2 border-white"
-            />
-          </div>
-          <!-- Komunitas Info -->
-          <div>
-            <p class="font-semibold text-gray-800">150+</p>
-            <p class="text-gray-500 text-sm">Join our active community</p>
-          </div>
-        </div>
       </div>
 
       <!-- Teks dan Tombol -->
